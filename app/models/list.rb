c697c7a3ea97f2f20d5b5eb03618030eda79ca06
Santiago Micolini <santi.micolini2@gmail.com>
2022-11-17 17:25:47 +0100

    movies list

---
+++ app/models/list.rb
@@ -0,0 +1,8 @@
+class List < ApplicationRecord
+  has_many :bookmarks
+  has_many :movies, through: :bookmarks, dependent: :destroy
+
+
+  validates :name, uniqueness: true
+  validates :name, presence: true
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,12 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
+  root to: 'lists#index'
+  resources :lists, only: [:index, :new, :create, :show] do
+    resources :bookmarks, only: [:new, :create]
+  end
+  # resources :lists, only: [:index]
+
   # Defines the root path route ("/")
   # root "articles#index"
 end
